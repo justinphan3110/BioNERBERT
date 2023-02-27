1bd3335a930ec557de106840f91a3c1d7f15bf5f
justinphan3110 <justinphan3110@gmail.com>
2023-02-26 19:07:33 -0500

    clean up some extra args

--- pretrain/pretrain.py
+++ pretrain/pretrain.py
@@ -618,13 +618,6 @@ def main():
         trainer.save_metrics("eval", metrics)
 
     kwargs = {"finetuned_from": model_args.model_name_or_path, "tasks": "fill-mask"}
-    if data_args.dataset_name is not None:
-        kwargs["dataset_tags"] = data_args.dataset_name
-        if data_args.dataset_config_name is not None:
-            kwargs["dataset_args"] = data_args.dataset_config_name
-            kwargs["dataset"] = f"{data_args.dataset_name} {data_args.dataset_config_name}"
-        else:
-            kwargs["dataset"] = data_args.dataset_name
 
     if training_args.push_to_hub:
         trainer.push_to_hub(**kwargs)
