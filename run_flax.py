44bb98279144bb069d6428f59bd7032b1af3b17b
justinphan3110 <justinphan3110@gmail.com>
2023-03-01 14:36:54 -0500

    update params

--- run_flax.py
+++ run_flax.py
@@ -8,11 +8,11 @@ os.environ['MODEL_PATH']=f"{os.environ['MODEL']}"
 os.environ['task']='pretraining'
 os.environ['datadir']=f"data"
 os.environ['outdir']=f"out/{os.environ['task']}/{os.environ['MODEL']}"
-os.environ["WANDB_DISABLED"] = "true"
+os.environ["wandb_project"] = "BioNERBERT"
 
 os.system(f'''python3 pretrain/run_mlm_flax.py --model_name_or_path {os.environ['MODEL_PATH']} \
     --train_file {os.environ['datadir']}/pubmed_abs_biolinkner_synthetic_data.json --do_train --do_eval \
-    --per_device_train_batch_size 128 --learning_rate 6e-4 \
-    --warmup_steps 100 --max_seq_length 512 --num_train_epochs 1 --preprocessing_num_workers 16 \
-    --save_strategy no --evaluation_strategy steps --eval_steps 500 --logging_steps 500 \
-     --output_dir {os.environ['outdir']} --cache_dir /mnt/disks/persist --overwrite_output_dir''')
+    --per_device_train_batch_size 128 --learning_rate 3e-4 \
+    --warmup_steps 5000 --max_seq_length 512 --num_train_epochs 1 --preprocessing_num_workers 64 \
+    --save_strategy no --evaluation_strategy steps --eval_steps 1000 --logging_steps 1000 \
+     --output_dir {os.environ['outdir']} --cache_dir /mnt/disks/persist --wandb_project {os.environ["wandb_project"]} --overwrite_output_dir''')
