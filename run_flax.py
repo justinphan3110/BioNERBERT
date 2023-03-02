5bbcb79a81c2228cdb326155803a2e796737f824
justinphan3110 <justinphan3110@gmail.com>
2023-03-02 02:03:08 -0500

    update hyper

--- run_flax.py
+++ run_flax.py
@@ -12,7 +12,7 @@ os.environ["wandb_project"] = "BioNERBERT"
 
 os.system(f'''python3 pretrain/run_mlm_flax.py --model_name_or_path {os.environ['MODEL_PATH']} \
     --train_file {os.environ['datadir']}/pubmed_abs_biolinkner_synthetic_data.json --do_train --do_eval \
-    --per_device_train_batch_size 128 --learning_rate 3e-4 \
+    --per_device_train_batch_size 32 --learning_rate 4e-4 \
     --warmup_steps 5000 --max_seq_length 512 --num_train_epochs 1 --preprocessing_num_workers 64 \
     --save_strategy no --evaluation_strategy steps --eval_steps 1000 --logging_steps 1000 \
      --output_dir {os.environ['outdir']} --cache_dir /mnt/disks/persist --wandb_project {os.environ["wandb_project"]} --overwrite_output_dir''')
--- set_up_tpu_flax.py
+++ set_up_tpu_flax.py
@@ -2,7 +2,5 @@ import os
 
 os.system('pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html')
 os.system('pip install --upgrade clu')
-os.system('pip install tensorflow')
-os.system('pip install tensorflow_datasets')
-os.system('git clone https://github.com/google/flax.git')
-os.system('pip install --user -e flax')
+os.system('pip install --upgrade git+https://github.com/google/flax.git')
+os.system('pip install -r requirements.txt')
